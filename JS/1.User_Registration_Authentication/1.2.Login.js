1363c2c622a9656ea8e89cfaa6a68ca1d73bda8d
Kalichhe <jhondh2003@gmail.com>
2024-08-27 23:50:29 -0500

    Melo con las validaciones del js

--- JS/1.User_Registration_Authentication/1.2.Login.js
+++ JS/1.User_Registration_Authentication/1.2.Login.js
@@ -1,14 +1,30 @@
 
 let validatePassword = (Password) => {
-    if (Password.length < 8) {
-        alert('Mano, la contraseña debe de ser mayor o igual a 8 caracteres')
+    if (Password == '') {
+        alert('Aaaa, entonces vale mia')
+    } else if (RepeatPassword == ''){
+        alert('Aaaa, ahora si se la gano')
+    } else if (Password.length < 12) {
+        alert('La contraseña debe de ser mayor o igual a 12 caracteres')
+    } else if (Password.length > 20) {
+        alert('La contraseña no puede ser mayor a 20 caracteres')
+    } else if (RepeatPassword.length < 12) {
+        alert('La contraseña debe de ser mayor o igual a 12 caracteres')
+    } else if (RepeatPassword.length > 20) {
+        alert('La contraseña no puede ser mayor a 20 caracteres')
     } else {
         return true;
     }
 }
 
 let validateUsername = (Username) => {
-    if (Username.length < 8) {
+    if (Username == '') {
+        alert('Parcero ingrese pues algo, ome')
+    } else if (Username.includes(' ')) {
+        alert('El Username no puede contener espacios');
+    } else if (!/^[a-zA-Z][a-zA-Z0-9]+$/.test(Username)) {
+        alert('El Username no puede contener caracteres especiales y no debe de iniciar con número');
+    } else if (Username.length < 8) {
         alert('El Username debe de ser mayor a 8 caracteres')
     } else if (Username.length > 15) {
         alert('El Username debe de ser menor a 15 caracteres')
